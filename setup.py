46da9c203ef9bdd61335ebbefc17a5c170cf8e0a
= <tript@vng.com.vn>
2019-06-04 22:18:15 +0700

    init

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="zaailabcorelib",
-    version="0.0.4",
+    version="0.0.5",
     author="tript",
     author_email="thanhtri2502@gmail.com",
     description="A common config for every project",
