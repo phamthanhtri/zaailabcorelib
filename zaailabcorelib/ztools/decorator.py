157f16597541fbec27bc9a92039969b8b250b305
congvm <congvm@vng.com.vn>
2019-11-26 14:38:24 +0700

    Add decorator

---
+++ zaailabcorelib/ztools/decorator.py
@@ -0,0 +1,33 @@
+import traceback
+from time import time
+
+def zlogging_deco(default_value, logger=None):
+    log_id = str(time())
+    def print_log(msg, log_level):
+        if logger is not None: 
+            assert log_level in ['info', 'exception', 'error', 'debug']
+            if log_level=='info':
+                logger.info(msg)
+            elif log_level=='exception':
+                logger.exception(msg)
+            elif log_level== 'error':
+                logger.exception(msg)
+            elif log_level== 'debug':
+                logger.debug(msg)
+        else: 
+            print(msg)   
+            
+    def nested(func):
+        def wrapper(*args, **kwargs):
+            print_log("[{}][Input] - {} - {} - log_id: {}".format(func.__name__.upper(), args, kwargs, log_id), log_level='info')
+            try:    
+                result = func(*args, **kwargs)
+                print_log("[{}][Result] - {} - log_id: {}".format(func.__name__.upper(), result, log_id), log_level='info')
+                if type(result) != type(default_value):
+                    print_log("[{}][Result][WARNING] - {} - log_id: {}".format(func.__name__.upper(), result, log_id), log_level='info')
+                return result
+            except:
+                print_log("[{}][Result] - {} - log_id: {}".format(func.__name__.upper(), traceback.format_exc(), log_id), log_level='exception')
+                return default_value
+        return wrapper
+    return nested
--- zaailabcorelib/ztools/image.py
+++ zaailabcorelib/ztools/image.py
@@ -2,13 +2,12 @@
 import cv2
 import numpy as np
 from PIL import Image
+import requests
+from io import BytesIO
+import os
 
 
-def load_image(path_to_load, mode='cv', channel_format='rgb'):
-    mode = mode.lower()
-    channel_format = channel_format.lower()
-    assert mode in ['cv', 'pil'], ValueError('`mode` must be in [\'cv\', \'pil\']')
-    assert channel_format in ['rgb', 'bgr'], ValueError('`mode` must be in [\'rgb\', \'bgr\']')
+def load_image_from_disk(path_to_load, mode='cv', channel_format='rgb'):
     if mode == 'cv':
         img_arr = cv2.imread(path_to_load)
         if channel_format == 'rgb':
@@ -23,6 +22,37 @@ def load_image(path_to_load, mode='cv', channel_format='rgb'):
         return img_pil
     
 
+def load_image_from_url(url, mode='cv', channel_format='rgb'):
+    response = requests.get(url)
+    img_pil = Image.open(BytesIO(response.content))
+    img_pil = img_pil.convert('RGB')
+    if mode == 'cv':
+        img_arr = np.array(img_pil, np.uint8)
+        if channel_format == 'bgr':
+            img_arr = cv2.cvtColor(img_arr, cv2.COLOR_RGB2BGR)
+        return img_arr
+
+    elif mode == 'pil':
+        if channel_format == 'bgr':
+            img_pil = img_pil.convert('BGR')
+        return img_pil
+    
+    
+def load_image(path_or_url, mode='cv', channel_format='rgb', proxy=None):
+    if proxy is not None:
+        os.environ['https_proxy'] = proxy
+        os.environ['http_proxy'] = proxy
+    path_or_url = path_or_url.lower()
+    mode = mode.lower()
+    channel_format = channel_format.lower()
+    assert mode in ['cv', 'pil'], ValueError('`mode` must be in [\'cv\', \'pil\']')
+    assert channel_format in ['rgb', 'bgr'], ValueError('`mode` must be in [\'rgb\', \'bgr\']')
+    if 'http://' in path_or_url or 'https://' in path_or_url:
+        return load_image_from_url(path_or_url, mode, channel_format)
+    else:
+        return load_image_from_disk(path_or_url, mode, channel_format)
+    
+
 def encode_image(image_arr, ext="jpg", format_params=None):
     img_as_string = cv2.imencode("." + ext, image_arr, params=format_params)[1].tostring()
     img_as_hex = img_as_string.hex()
@@ -33,3 +63,19 @@ def decode_image(img_as_hex):
     img_as_bytes = bytes.fromhex(img_as_hex)
     img_arr = cv2.imdecode(np.frombuffer(img_as_bytes, dtype=np.uint8), 1)
     return img_arr
+
+
+
+# import sys
+# import  matplotlib.pyplot as plt
+# sys.path.append('/media/congvm/DATA/Workspace/zaailabcorelib/zaailabcorelib/ztools/')
+# sys.path.append('/media/congvm/DATA/Workspace/zaailabcorelib/example/')
+
+# from image import load_image
+# url = "http://www.emmc-imae.org/wp-content/uploads/2012/11/slider2.jpg"
+# img = load_image(url)
+# print(img)
+# plt.imshow(img)
+
+
+# img = load_image('/media/congvm/DATA/Workspace/zaailabcorelib/example/slider2.jpg')
