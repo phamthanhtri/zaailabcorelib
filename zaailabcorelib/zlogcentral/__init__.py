0938811e61bac53dfe146f027e869f295dc424f3
tinhdm <tinhdm@vng.com.vn>
2020-05-08 09:54:38 +0700

    add log client

---
+++ zaailabcorelib/zlogcentral/__init__.py
@@ -0,0 +1,2 @@
+from zaailabcorelib.zlogcentral.api import LogClient
+Zlogcentral = LogClient("10.40.34.21",9999)
---
+++ zaailabcorelib/zlogcentral/api.py
@@ -0,0 +1,14 @@
+import requests
+class LogClient:
+
+    def __init__(self, host, port, path="/write_log"):
+        self.host = host
+        self.port = str(port)
+        self.path = path
+    def log(self, category, log):
+        data = {
+            'category': category,
+            'log': log
+                }
+        res = requests.post("http://"+self.host+":"+self.port+self.path, data)
+        return res
