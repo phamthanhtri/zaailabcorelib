575287bef16a3b8377e029fac4bc03cf14a69729
tinhdm <tinhdm@vng.com.vn>
2020-05-12 17:46:36 +0700

    version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="zaailabcorelib",
-    version="0.1.9.7",
+    version="0.1.9.8",
     author="ailabteam",
     include_package_data=True,
     description="A useful tools inside ZAI Lab",
