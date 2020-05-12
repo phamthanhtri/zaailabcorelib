1601db89232855eb7597330922e3799b9e282204
tinhdm <tinhdm@vng.com.vn>
2020-05-12 17:30:29 +0700

    format log

--- zaailabcorelib/zlogcentral/api.py
+++ zaailabcorelib/zlogcentral/api.py
@@ -1,11 +1,26 @@
 import grequests
+import socket
+import time
+
+
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    local_ip = s.getsockname()[0]
+    s.close()
+    return local_ip
+
 class LogClient:
 
     def __init__(self, host, port, path="/log"):
         self.host = host
         self.port = str(port)
         self.path = path
+
     def log(self, category, log):
+        local_ip = get_local_ip()
+        created_time = int(time.time())
+        log = local_ip + '\t' + str(created_time) + '\t' + log
         data = {
             'category': category,
             'log': log
