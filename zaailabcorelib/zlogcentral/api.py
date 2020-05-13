cf0663d1c40493bc020e9fcf12dfb16d7942c347
tinhdm <tinhdm@vng.com.vn>
2020-05-13 11:21:31 +0700

    add name of folder

--- zaailabcorelib/zlogcentral/api.py
+++ zaailabcorelib/zlogcentral/api.py
@@ -2,12 +2,17 @@ import socket
 import time
 import requests
 from grequests import Pool
+import os
+import json
 request_pool = Pool(20)
 def get_local_ip():
     local_ip = socket.gethostname()
     local_ip="10.40.34."+local_ip[-2:]
     return local_ip
-
+def get_name_of_folder():
+    path = os.getcwd()
+    folder = os.path.basename(path)
+    return folder
 class LogClient:
 
     def __init__(self, host, port):
@@ -21,9 +26,18 @@ class LogClient:
             pass
 
     def __general_log(self, category, log, path):
+        project = get_name_of_folder()
         local_ip = get_local_ip()
-        created_time = int(time.time())
-        log = local_ip + '\t' + str(created_time) + '\t' + log
+        created_time = int(time.time()*1000)
+        log_json={
+            "project":project,
+            "ip":local_ip,
+            "created_time":created_time,
+            "log":log
+
+        }
+
+        log = json.dumps(log_json)
         data = {
             'category': category,
             'log': log
