0f3b63f41ac2795e7ee76994b3b7b835c6a16f5c
tinhdm <tinhdm@vng.com.vn>
2020-05-13 08:43:20 +0700

    fix send async

--- zaailabcorelib/zlogcentral/api.py
+++ zaailabcorelib/zlogcentral/api.py
@@ -1,29 +1,33 @@
 import grequests
 import socket
 import time
-
-
+import requests
 def get_local_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    local_ip = s.getsockname()[0]
-    s.close()
+    local_ip = socket.gethostname()
+    local_ip="10.40.34."+local_ip[-2:]
     return local_ip
 
 class LogClient:
 
-    def __init__(self, host, port, path="/log"):
+    def __init__(self, host, port):
         self.host = host
         self.port = str(port)
-        self.path = path
 
-    def log(self, category, log):
+    def __send_request(self, param):
+        requests.post(param[0], data=param[1])
+
+    def __general_log(self, category, log, path):
         local_ip = get_local_ip()
         created_time = int(time.time())
         log = local_ip + '\t' + str(created_time) + '\t' + log
         data = {
             'category': category,
             'log': log
-                }
-        res = grequests.post("http://"+self.host+":"+self.port+self.path, data = data)
-        return res
+        }
+        param =[]
+        param.append("http://" + self.host + ":" + self.port + path)
+        param.append(data)
+        pool = grequests.Pool(1)
+        pool.spawn(self.__send_request,param)
+    def log(self, category, log):
+        self.__general_log(category, log, "/log")
