0dc68d94f4ee18bb14e20787c360bdee5a18df21
tinhdm <tinhdm@vng.com.vn>
2020-05-29 13:58:04 +0700

    update requirement

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from distutils.core import setup
 import setuptools
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
+install_reqs = parse_requirements("requirements.txt")
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="zaailabcorelib",
-    version="0.2.0.6",
+    version="0.2.0.7",
     author="ailabteam",
     include_package_data=True,
     description="A useful tools inside ZAI Lab",
@@ -20,4 +27,5 @@ setuptools.setup(
     ],
     tests_require=["pytest", "mock"],
     test_suite="pytest",
+    install_requires=install_reqs,
 )
